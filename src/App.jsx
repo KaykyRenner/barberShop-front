c0df123c8893d41a47a50238d569a0eee67cd24e
Kayky Renner Siqueira Galindo <kayky.renner.sg@gmail.com>
2025-02-25 13:56:18 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+function App() {
+  return (
+    <>
+      <div>
+      
+      </div>
+      <p className="read-the-docs">
+        BARBERSHOP
+      </p>
+    </>
+  )
+}
+
+export default App
